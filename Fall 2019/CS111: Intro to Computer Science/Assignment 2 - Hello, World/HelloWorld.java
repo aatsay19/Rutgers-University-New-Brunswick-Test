d96ec498f1c7f42af68ed99d1a31d2b85c27c913
aatsay19 <49285804+aatsay19@users.noreply.github.com>
2021-08-12 16:44:42 -0400

    Uploaded HelloWorld solution
    I uploaded the Java file that contains the solution to the first part of this assignment (the Hello World problem).

---
+++ Fall 2019/CS111: Intro to Computer Science/Assignment 2 - Hello, World/HelloWorld.java
@@ -0,0 +1,24 @@
+/*************************************************************************
+ *  Compilation:  javac HelloWorld.java
+ *  Execution:    java HelloWorld
+ *
+ *  @author:  Aatif Sayed
+ *	 NetID :  as3259
+ *	 E-mail:  as3259@scarletmail.rutgers.edu
+ *
+ *  Prints "Hello, World". By tradition, this is everyone's first program.
+ *
+ *  % java HelloWorld
+ *  Hello, World
+ *
+ *************************************************************************/
+
+public class HelloWorld {
+  
+    public static void main(String[] args) {
+      
+		  System.out.println("Hello, World");
+      
+    }
+  
+}
